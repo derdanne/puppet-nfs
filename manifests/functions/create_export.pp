8342be50e4cfe6adf28282a12fcc8a4af7a0e9d3
Brian Schonecker <brian.schonecker@nfiindustries.com>
2023-08-09 08:04:32 -0400

    Use edge-filepath module to create parent directories for NFS exports.

--- manifests/functions/create_export.pp
+++ manifests/functions/create_export.pp
@@ -50,12 +50,11 @@ define nfs::functions::create_export (
     }
 
     unless defined(File[$name]) {
-      file { $name:
-        ensure                  => directory,
+      filepath { $name:
+        ensure                  => present,
         owner                   => $owner,
         group                   => $group,
         mode                    => $mode,
-        selinux_ignore_defaults => true,
       }
     }
   }
